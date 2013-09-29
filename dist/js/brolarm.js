84dc957907fe7856ed21672354db31757e87a0d2
Justin Petersen <justin@justinpetersen.com>
2013-09-29 14:43:09 -0700

    Displaying frineds list

--- dist/js/brolarm.js
+++ dist/js/brolarm.js
@@ -1,290 +1,6 @@
-// An example Backbone application contributed by
-// [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses a simple
-// [LocalStorage adapter](backbone-localstorage.html)
-// to persist Backbone models within your browser.
-
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
-
-  // Todo Model
-  // ----------
-
-  // Our basic **Todo** model has `title`, `order`, and `done` attributes.
-  var Todo = Backbone.Model.extend({
-
-    // Default attributes for the todo item.
-    defaults: function() {
-      return {
-        title: "empty todo...",
-        order: Todos.nextOrder(),
-        done: false
-      };
-    },
-
-    // Toggle the `done` state of this todo item.
-    toggle: function() {
-      this.save({done: !this.get("done")});
-    }
-
-  });
-
-  // Todo Collection
-  // ---------------
-
-  // The collection of todos is backed by *localStorage* instead of a remote
-  // server.
-  var TodoList = Backbone.Collection.extend({
-
-    // Reference to this collection's model.
-    model: Todo,
-
-    // Save all of the todo items under the `"todos-backbone"` namespace.
-    localStorage: new Backbone.LocalStorage("todos-backbone"),
-
-    // Filter down the list of all todo items that are finished.
-    done: function() {
-      return this.where({done: true});
-    },
-
-    // Filter down the list to only todo items that are still not finished.
-    remaining: function() {
-      return this.where({done: false});
-    },
-
-    // We keep the Todos in sequential order, despite being saved by unordered
-    // GUID in the database. This generates the next order number for new items.
-    nextOrder: function() {
-      if (!this.length) return 1;
-      return this.last().get('order') + 1;
-    },
-
-    // Todos are sorted by their original insertion order.
-    comparator: 'order'
-
-  });
-
-  // Create our global collection of **Todos**.
-  var Todos = new TodoList( );
-
-  // Todo Item View
-  // --------------
-
-  // The DOM element for a todo item...
-  var TodoView = Backbone.View.extend({
-
-    //... is a list tag.
-    tagName:  "li",
-
-    // Cache the template function for a single item.
-    template: _.template($('#item-template').html()),
-
-    // The DOM events specific to an item.
-    events: {
-      "click .toggle"   : "toggleDone",
-      "dblclick .view"  : "edit",
-      "click a.destroy" : "clear",
-      "keypress .edit"  : "updateOnEnter",
-      "blur .edit"      : "close"
-    },
-
-    // The TodoView listens for changes to its model, re-rendering. Since there's
-    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
-    // app, we set a direct reference on the model for convenience.
-    initialize: function() {
-      this.listenTo(this.model, 'change', this.render);
-      this.listenTo(this.model, 'destroy', this.remove);
-    },
-
-    // Re-render the titles of the todo item.
-    render: function() {
-      this.$el.html(this.template(this.model.toJSON()));
-      this.$el.toggleClass('done', this.model.get('done'));
-      this.input = this.$('.edit');
-      return this;
-    },
-
-    // Toggle the `"done"` state of the model.
-    toggleDone: function() {
-      this.model.toggle();
-    },
-
-    // Switch this view into `"editing"` mode, displaying the input field.
-    edit: function() {
-      this.$el.addClass("editing");
-      this.input.focus();
-    },
-
-    // Close the `"editing"` mode, saving changes to the todo.
-    close: function() {
-      var value = this.input.val();
-      if (!value) {
-        this.clear();
-      } else {
-        this.model.save({title: value});
-        this.$el.removeClass("editing");
-      }
-    },
-
-    // If you hit `enter`, we're through editing the item.
-    updateOnEnter: function(e) {
-      if (e.keyCode == 13) this.close();
-    },
-
-    // Remove the item, destroy the model.
-    clear: function() {
-      this.model.destroy();
-    }
-
-  });
-
-  // The Application
-  // ---------------
-
-  // Our overall **AppView** is the top-level piece of UI.
-  var AppView = Backbone.View.extend({
-
-    // Instead of generating a new element, bind to the existing skeleton of
-    // the App already present in the HTML.
-    el: $("#todoapp"),
-
-    // Our template for the line of statistics at the bottom of the app.
-    statsTemplate: _.template($('#stats-template').html()),
-
-    // Delegated events for creating new items, and clearing completed ones.
-    events: {
-      "keypress #new-todo":  "createOnEnter",
-      "click #clear-completed": "clearCompleted",
-      "click #toggle-all": "toggleAllComplete"
-    },
-
-    // At initialization we bind to the relevant events on the `Todos`
-    // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved in *localStorage*.
-    initialize: function() {
-
-      this.input = this.$("#new-todo");
-      this.allCheckbox = this.$("#toggle-all")[0];
-
-      this.listenTo(Todos, 'add', this.addOne);
-      this.listenTo(Todos, 'reset', this.addAll);
-      this.listenTo(Todos, 'all', this.render);
-
-      this.footer = this.$('footer');
-      this.main = $('#main');
-
-      Todos.fetch();
-    },
-
-    // Re-rendering the App just means refreshing the statistics -- the rest
-    // of the app doesn't change.
-    render: function() {
-      var done = Todos.done().length;
-      var remaining = Todos.remaining().length;
-
-      if (Todos.length) {
-        this.main.show();
-        this.footer.show();
-        this.footer.html(this.statsTemplate({done: done, remaining: remaining}));
-      } else {
-        this.main.hide();
-        this.footer.hide();
-      }
-
-      this.allCheckbox.checked = !remaining;
-    },
-
-    // Add a single todo item to the list by creating a view for it, and
-    // appending its element to the `<ul>`.
-    addOne: function(todo) {
-      var view = new TodoView({model: todo});
-      this.$("#todo-list").append(view.render().el);
-    },
-
-    // Add all items in the **Todos** collection at once.
-    addAll: function() {
-      Todos.each(this.addOne, this);
-    },
-
-    // If you hit return in the main input field, create new **Todo** model,
-    // persisting it to *localStorage*.
-    createOnEnter: function(e) {
-      if (e.keyCode != 13) return;
-      if (!this.input.val()) return;
-
-      Todos.create({title: this.input.val()});
-      this.input.val('');
-    },
-
-    // Clear all done todo items, destroying their models.
-    clearCompleted: function() {
-      _.invoke(Todos.done(), 'destroy');
-      return false;
-    },
-
-    toggleAllComplete: function () {
-      var done = this.allCheckbox.checked;
-      Todos.each(function (todo) { todo.save({'done': done}); });
-    }
-
-  });
-
-  var GamerProfileView = Backbone.View.extend({
-    
-    template: _.template( $( '#gamer-profile-template' ).html( ) ),
-    
-    initialize: function( ) {
-      
-      console.log( 'GamerProfileView.initialize( )' );
-      
-      this.listenTo( this.model, 'change', this.render );
-      
-    },
-
-    render: function( ) {
-      
-      console.log( 'GamerProfileView.render( )' );
-      
-      this.$el.html( this.template( this.model.toJSON( ) ) );
-      
-      return this;
-      
-    }
-
-  });
-
-  var GamerProfileModel = Backbone.Model.extend({
-
-    urlRoot: 'http://www.xboxleaders.com/api/2.0/profile.json',
-
-    url: function( ) {
-
-      return this.urlRoot + '?gamertag=' + this.get( 'gamertag' );
-
-    },
-
-    defaults: function( ) {
-      return {
-        gamertag: '',
-        avatar: '',
-        presence: '',
-        online: false
-      };
-    },
-
-    parse: function( response, options ) {
-
-      console.log( 'BroLarmModel.parse( )' );
-      return {
-        gamertag: response.data.gamertag,
-        avatar: response.data.avatar.tile,
-        presence: response.data.presence,
-        online: response.data.online
-      };
-
-    }
-
-  });
-
+  
   var FriendModel = Backbone.Model.extend({
 
     urlRoot: 'http://www.xboxleaders.com/api/2.0/profile.json',
@@ -307,27 +23,116 @@ $(function(){
     parse: function( response, options ) {
 
       console.log( 'FriendModel.parse( )' );
+      
+      return {
+        gamertag: response.gamertag,
+        avatar: response.gamerpic.large,
+        presence: response.status,
+        online: response.online
+      };
+
+    }
+
+  });
+
+  var FriendsCollection = Backbone.Collection.extend({
+
+    model: FriendModel,
+    
+    gamerProfileModel: null,
+
+    urlRoot: 'http://www.xboxleaders.com/api/2.0/friends.json',
+    
+    initialize: function( gamerProfileModel ) {
+      
+      console.log( 'FriendsCollection.initialize( )' );
+      
+      this.gamerProfileModel = gamerProfileModel;
+      
+    },
+    
+    parse: function( response, options ) {
+      
+      console.log( 'FriendsCollection.parse( )' );
+      
+      return response.data.friends;
+      
+    },
+
+    url: function( ) {
+
+      return this.urlRoot + '?gamertag=' + this.gamerProfileModel.get( 'gamertag' );
+
+    }
+
+  });
+
+  var GamerProfileModel = Backbone.Model.extend({
+
+    urlRoot: 'http://www.xboxleaders.com/api/2.0/profile.json',
+
+    initialize: function( ) {
+
+      console.log( 'GamerProfileModel.initialize( )' );
+      
+      this.friendsCollection = new FriendsCollection( this );
+
+    },
+
+    url: function( ) {
+
+      return this.urlRoot + '?gamertag=' + this.get( 'gamertag' );
+
+    },
+
+    defaults: function( ) {
+      return {
+        gamertag: '',
+        avatar: '',
+        presence: '',
+        online: false
+      };
+    },
+
+    parse: function( response, options ) {
+
+      console.log( 'GamerProfileModel.parse( )' );
+      
+      this.friendsCollection.fetch( );
+
       return {
         gamertag: response.data.gamertag,
-        avatar: response.data.gamerpic.small,
-        presence: response.data.status,
+        avatar: response.data.avatar.tile,
+        presence: response.data.presence,
         online: response.data.online
       };
 
     }
 
   });
-  
-  var FriendsList = Backbone.Collection.extend({
 
-    model: FriendModel,
+  var GamerProfileView = Backbone.View.extend({
+    
+    template: _.template( $( '#gamer-profile-template' ).html( ) ),
+    
+    tagName: 'li',
+    
+    initialize: function( ) {
+      
+      console.log( 'GamerProfileView.initialize( )' );
+      
+      this.listenTo( this.model, 'change', this.render );
+      
+    },
 
-    urlRoot: 'http://www.xboxleaders.com/api/2.0/friends.json',
-
-    url: function( ) {
-
-      return this.urlRoot + '?gamertag=' + this.get( 'gamertag' );
-
+    render: function( ) {
+      
+      console.log( 'GamerProfileView.render( )' );
+      
+      this.$el.html( this.template( this.model.toJSON( ) ) );
+      
+      return this;
+      
     }
 
   });
@@ -370,7 +175,16 @@ $(function(){
 
       this.stopLoader( );
       this.renderGamerProfile( );
+      this.clearFriends( );
 
+    },
+    
+    onFriendAdded: function( model ) {
+      
+      console.log( 'BroLarmView.onFriendAdded( )' );
+      
+      this.renderFriend( model );
+      
     },
 
     initialize: function( ) {
@@ -380,6 +194,7 @@ $(function(){
       this.input = $( '#gamertag' );
       this.laddaSubmit = Ladda.create( $( '#form-submit' )[ 0 ] );
       this.listenTo( this.model, 'change', this.onModelChanged );
+      this.listenTo( this.model.friendsCollection, 'add', this.onFriendAdded );
       
       this.validateForm( );
 
@@ -399,6 +214,27 @@ $(function(){
       
       var view = new GamerProfileView( { model: this.model } );
       $( '#gamer-profile-container' ).html( view.render( ).el );
+      
+      if ( this.model.get( 'avatar' ) != '' ) {
+        $( '#gamer-profile-container' ).show( );
+      } else {
+        $( '#gamer-profile-container' ).hide( );
+      }
+      
+    },
+    
+    renderFriend: function( model ) {
+      
+      $( '#friends-heading' ).show( );
+      var view = new GamerProfileView( { model: model } );
+      $( '#friends-container' ).append( view.render().el );
+      
+    },
+    
+    clearFriends: function( ) {
+      
+      $( '#friends-heading' ).hide( );
+      $( '#friends-container' ).empty( );
       
     },
     
@@ -451,7 +287,7 @@ $(function(){
 
   // var App = new AppView( );
 
-  var model = new GamerProfileModel( );
-  var view = new BroLarmView( { model: model } );
+  var gamerProfileModel = new GamerProfileModel( );
+  var view = new BroLarmView( { model: gamerProfileModel } );
 
 });
