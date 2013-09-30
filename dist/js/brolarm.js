57d029f6a1affd2da16f82e86bbcc4f76bb2c471
Justin Petersen <justin.petersen@SFM0LPF1G4L.local>
2013-09-30 13:24:02 -0700

    Improved gamer profile item formatting

--- dist/js/brolarm.js
+++ dist/js/brolarm.js
@@ -1,6 +1,6 @@
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
-  
+
   var FriendModel = Backbone.Model.extend({
 
     urlRoot: 'http://www.xboxleaders.com/api/2.0/profile.json',
@@ -23,7 +23,7 @@ $(function(){
     parse: function( response, options ) {
 
       console.log( 'FriendModel.parse( )' );
-      
+
       return {
         gamertag: response.gamertag,
         avatar: response.gamerpic.large,
@@ -38,25 +38,25 @@ $(function(){
   var FriendsCollection = Backbone.Collection.extend({
 
     model: FriendModel,
-    
+
     gamerProfileModel: null,
 
     urlRoot: 'http://www.xboxleaders.com/api/2.0/friends.json',
-    
+
     initialize: function( gamerProfileModel ) {
-      
+
       console.log( 'FriendsCollection.initialize( )' );
-      
+
       this.gamerProfileModel = gamerProfileModel;
-      
+
     },
-    
+
     parse: function( response, options ) {
-      
+
       console.log( 'FriendsCollection.parse( )' );
-      
+
       return response.data.friends;
-      
+
     },
 
     url: function( ) {
@@ -74,7 +74,7 @@ $(function(){
     initialize: function( ) {
 
       console.log( 'GamerProfileModel.initialize( )' );
-      
+
       this.friendsCollection = new FriendsCollection( this );
 
     },
@@ -97,7 +97,7 @@ $(function(){
     parse: function( response, options ) {
 
       console.log( 'GamerProfileModel.parse( )' );
-      
+
       this.friendsCollection.fetch( );
 
       return {
@@ -112,27 +112,29 @@ $(function(){
   });
 
   var GamerProfileView = Backbone.View.extend({
-    
+
     template: _.template( $( '#gamer-profile-template' ).html( ) ),
-    
+
     tagName: 'li',
-    
+
+    className: 'list-group-item',
+
     initialize: function( ) {
-      
+
       console.log( 'GamerProfileView.initialize( )' );
-      
+
       this.listenTo( this.model, 'change', this.render );
-      
+
     },
 
     render: function( ) {
-      
+
       console.log( 'GamerProfileView.render( )' );
-      
+
       this.$el.html( this.template( this.model.toJSON( ) ) );
-      
+
       return this;
-      
+
     }
 
   });
@@ -149,10 +151,10 @@ $(function(){
     onGamertagKeyPress: function( e ) {
 
       console.log( 'BroLarmView.onGamertagEnter( )' );
-      
+
       // Validate the form on any key press
       var valid = this.validateForm( );
-      
+
       // If the enter key is pressed and the form is valid, then load the gamer profile
       if ( e.keyCode == 13 && valid ) {
         this.loadGamerProfile( this.input.val( ) );
@@ -178,24 +180,24 @@ $(function(){
       this.clearFriends( );
 
     },
-    
+
     onFriendAdded: function( model ) {
-      
+
       console.log( 'BroLarmView.onFriendAdded( )' );
-      
+
       this.renderFriend( model );
-      
+
     },
 
     initialize: function( ) {
 
       console.log( 'BroLarmView.initialize( )' );
-      
+
       this.input = $( '#gamertag' );
       this.laddaSubmit = Ladda.create( $( '#form-submit' )[ 0 ] );
       this.listenTo( this.model, 'change', this.onModelChanged );
       this.listenTo( this.model.friendsCollection, 'add', this.onFriendAdded );
-      
+
       this.validateForm( );
 
     },
@@ -207,42 +209,42 @@ $(function(){
       return this;
 
     },
-    
+
     renderGamerProfile: function( ) {
 
       console.log( 'BroLarmView.renderGamerProfile( )' );
-      
+
       var view = new GamerProfileView( { model: this.model } );
       $( '#gamer-profile-container' ).html( view.render( ).el );
-      
+
       if ( this.model.get( 'avatar' ) != '' ) {
         $( '#gamer-profile-container' ).show( );
       } else {
         $( '#gamer-profile-container' ).hide( );
       }
-      
+
     },
-    
+
     renderFriend: function( model ) {
-      
+
       $( '#friends-heading' ).show( );
       var view = new GamerProfileView( { model: model } );
       $( '#friends-container' ).append( view.render().el );
-      
+
     },
-    
+
     clearFriends: function( ) {
-      
+
       $( '#friends-heading' ).hide( );
       $( '#friends-container' ).empty( );
-      
+
     },
-    
+
     validateForm: function( ) {
-      
+
       // Assume that the form entries are valid
       var valid = true;
-      
+
       // Check that the form entries are valid and disable/enable the submit button
       if ( this.input.val( ).length < 3 ) {
         valid = false;
@@ -250,23 +252,23 @@ $(function(){
       } else {
         $( '#form-submit' ).removeAttr('disabled');
       }
-      
+
       return valid;
-      
+
     },
-    
+
     loadGamerProfile: function( gamertag ) {
 
       // Reset the gamer profile to its defaults
       this.model.set( this.model.defaults( ) );
-      
+
       // Set the user's gamertag in the model
       this.model.set( 'gamertag', gamertag );
 
       // Fetch the user profile
       this.startLoader( );
       this.model.fetch( );
-      
+
     },
 
     startLoader: function( ) {
