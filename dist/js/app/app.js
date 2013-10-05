82e2871047f78df233e99dc8fc2e63d2289e8499
Justin Petersen <justin@justinpetersen.com>
2013-10-05 09:42:54 -0700

    Renamed brolarm.js to brolarmapp.js

---
+++ dist/js/app/app.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  var BroLarm.Views.Controller = Backbone.View.extend({
+  
+    initialize: function( ) {
+      
+      console.log( 'BroLarm.Views.Controller.initialize( )' );
+    
+    }
+
+  });
+
+})();
