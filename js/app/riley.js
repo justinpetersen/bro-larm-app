03533a88d2f6f89d1b82fa1a4d7718751df40505
Justin Petersen <justin@justinpetersen.com>
2013-11-05 20:26:23 -0800

    Returning friends online status

--- js/app/riley.js
+++ js/app/riley.js
@@ -7,6 +7,8 @@ exports.getFriendsOnline = function(req, res) {
   broLarmUser.on('value', function(data) {
       var gamertag = data.val().gamertag;
       var selectedFriends = data.val().selectedFriends;
+      console.log(gamertag + ": " + selectedFriends);
+      
       var selectedFriendsLookup = {};
       for (var i=0; i<selectedFriends.length; i++) {
         selectedFriendsLookup[selectedFriends[i]] = true;
@@ -14,14 +16,16 @@ exports.getFriendsOnline = function(req, res) {
 
       $ = require('jquery');
       $.getJSON('https://www.xboxleaders.com/api/2.0/friends.json?gamertag=Major%20Nelson', function(data) {
+        // Assume all this gamer's friends are offline.
+        var online = false;
         for (var i=0; i<data.data.friends.length; i++) {
-          if (data.data.friends[i].status == ONLINE_STATUS) {
-            res.json(200, {'status': 'online'});
-            return;
+          if (selectedFriendsLookup[data.data.friends[i].gamertag] && data.data.friends[i].status.indexOf(ONLINE_STATUS) != -1) {
+            online = true;
           }
         }
 
-        res.json(200, {'status': 'offline'});
+        var onlineStatus = online ? 'online' : 'offline';
+        res.json(200, {'status': onlineStatus});
       });
   });
 };
