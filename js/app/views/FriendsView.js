235867c75646e55dbdd28b4624bc43d44a90a22f
Justin Petersen <justin@justinpetersen.com>
2013-11-13 08:58:07 -0800

    Added padding to account for top nav height

--- js/app/views/FriendsView.js
+++ js/app/views/FriendsView.js
@@ -22,20 +22,32 @@ $(function(){
       // TODO: Look at the best events to trigger clearing the friends list
       this.listenTo(this.collection, 'request', $.proxy(this.onFriendsReset, this));
       this.listenTo(this.collection, 'reset', $.proxy(this.onFriendsReset, this));
-
+      
       this.render();
+    },
+    
+    initSpinner: function() {
+      $('#friends-spinner').waiting({ 
+          className: 'waiting-circles', 
+          elements: 8, 
+          radius: 20, 
+          auto: true 
+      });
     },
 
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
+      
+      this.initSpinner();
 
       return this;
     },
     
     renderFriend: function(model) {
+      $('#friends-spinner-container').hide();
+      
       var view = new brolarm.view.FriendItemView({ model: model });
       $('#friend-list').append(view.render().el);
-
     },
     
     clearFriends: function() {
